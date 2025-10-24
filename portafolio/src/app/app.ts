f9e14d6500fbecc15874899458e65315aee253d6
CardenasStudent <r4cf@outlook.com>
2025-10-24 12:32:48 -0500

    add rutes

--- portafolio/src/app/app.ts
+++ portafolio/src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { Header } from '../app/shared/header/header';
 import { Footer } from './shared/footer/footer';
+
 import { Portafolio } from './pages/portafolio/portafolio';
 import { About } from './pages/about/about';
 import { Item } from './pages/item/item';
 
+import { AppRoutingModule } from './app-routing';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer,Portafolio, About, Item],
+  imports: [RouterOutlet, Header, Footer,Portafolio, About, Item, AppRoutingModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
